1e02069d471ff1dc03b58d9c98a0c0481d1e7c0e
developer-sahadat <mdridoysikder862@gmail.com>
2022-04-19 15:20:09 +0600

    card added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Home/Home/Home";
+import Products from "./Components/Pages/Products/Products";
+import Services from "./Components/Pages/Services/Services";
+import SignUp from "./Components/Pages/SignUp/SignUp";
+
 import Header from "./Components/Shear/Header/Header";
 
 function App() {
@@ -9,6 +13,9 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/services" element={<Services />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
       </Routes>
     </div>
   );
--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Card from "../../Card/Card";
 import HeroSection from "../HeroSections/HeroSection";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <Card />
     </>
   );
 };
